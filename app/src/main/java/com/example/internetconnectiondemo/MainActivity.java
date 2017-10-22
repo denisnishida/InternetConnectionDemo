42cfe83652e83a7a6f4f73a0ee23f0c01db7e4da
Denis Nishida <denis.nishida@gmail.com>
2017-10-22 17:05:25 -0500

    Created button that checks connection and a individual method

--- app/src/main/java/com/example/internetconnectiondemo/MainActivity.java
+++ app/src/main/java/com/example/internetconnectiondemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -22,6 +23,11 @@ public class MainActivity extends AppCompatActivity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    checkInternetConnection();
+  }
+
+  private void checkInternetConnection()
+  {
     if (isNetworkAvailable())
     {
       Toast.makeText(this, "Internet is working", Toast.LENGTH_SHORT).show();
@@ -30,5 +36,10 @@ public class MainActivity extends AppCompatActivity
     {
       Toast.makeText(this, "No internet connection found", Toast.LENGTH_SHORT).show();
     }
+  }
+
+  public void onButtonClick(View view)
+  {
+    checkInternetConnection();
   }
 }
