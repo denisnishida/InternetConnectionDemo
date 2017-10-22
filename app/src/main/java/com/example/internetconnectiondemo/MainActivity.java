3ac4136b936ffaec938d1d8dc10414fd0c45a38a
Denis Nishida <denis.nishida@gmail.com>
2017-10-22 16:48:01 -0500

    Added internet permissions, created method that verifies the connection and test of it in OnCreate

--- app/src/main/java/com/example/internetconnectiondemo/MainActivity.java
+++ app/src/main/java/com/example/internetconnectiondemo/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.internetconnectiondemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+  private boolean isNetworkAvailable()
+  {
+    ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    if (isNetworkAvailable())
+    {
+      Toast.makeText(this, "Internet is working", Toast.LENGTH_SHORT).show();
+    }
+    else
+    {
+      Toast.makeText(this, "No internet connection found", Toast.LENGTH_SHORT).show();
+    }
   }
 }
