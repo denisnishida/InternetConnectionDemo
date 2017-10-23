03ddd8bece9eb2cdb91d580549bbb91465a13a34
Denis Nishida <denis.nishida@gmail.com>
2017-10-23 10:48:26 -0500

    Added textview to show type and modified methods to print all the network info

--- app/src/main/java/com/example/internetconnectiondemo/MainActivity.java
+++ app/src/main/java/com/example/internetconnectiondemo/MainActivity.java
@@ -5,17 +5,19 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
-  private boolean isNetworkAvailable()
+  private NetworkInfo getNetworkInfo()
   {
     ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
     NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+//    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    return activeNetworkInfo;
   }
 
   @Override
@@ -30,16 +32,32 @@ public class MainActivity extends AppCompatActivity
   private void checkInternetConnection()
   {
     TextView textView = (TextView)findViewById(R.id.textView);
+    TextView textView2 = (TextView)findViewById(R.id.textView2);
 
-    if (isNetworkAvailable())
+    NetworkInfo activeNetworkInfo = getNetworkInfo();
+
+    if (activeNetworkInfo != null && activeNetworkInfo.isConnected())
     {
       Toast.makeText(this, "Internet is working", Toast.LENGTH_SHORT).show();
-      textView.setText("Network Status: Connected");
+      Log.d("Demo", activeNetworkInfo.toString());
+
+//      textView.setText("Network Status: Connected");
+      textView.setText(activeNetworkInfo.toString().replace(',', '\n').replace('[', '').replace(']',''));
+
+      if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI)
+      {
+        textView2.setText("Type: Wifi");
+      }
+      else if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+      {
+        textView2.setText("Type: Mobile");
+      }
     }
     else
     {
       Toast.makeText(this, "No internet connection found", Toast.LENGTH_SHORT).show();
       textView.setText("Network Status: Not Connected");
+      textView2.setText("");
     }
   }
 
